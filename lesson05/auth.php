04be7e7581346da34055df8790075b2d2d036ee1
Grigory <grigory.e@gmail.com>
2015-03-18 13:17:19 +0300

    Work in progress....

--- lesson05/auth.php
+++ lesson05/auth.php
@@ -1,3 +1,14 @@
 <?php
+$users = ['Петр' => '112233', 'Мария' => '334455'];
+
+//Проверим есть ли данный пользователь вообще.
+
+if (array_key_exists($_POST['id'],$users)) {
+    echo 'Существует';
+    header('Location:authusers.html');
+} else {
+    header('Location:index.html');
+};
+
 
 ?>
---
+++ lesson05/indx.php
@@ -0,0 +1,64 @@
+<?php
+if (isset($_COOKIE['id'])) {
+    $id = $_COOKIE['id'];
+} else {
+    header ('Location:index.html');
+    exit;
+}
+?><!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Главная</title>
+    <style>
+        #regform {
+            width: 300px;
+            height: 200px;
+            margin: 0px auto;
+        }
+
+        #regform fieldset, legend {
+            border: solid 2px brown;
+        }
+
+        #regform input[type="text"] {
+            border: solid 2px #cccccc;
+        }
+
+        #regform input[type="text"]:hover {
+            border: solid 2px blueviolet;
+        }
+        #regform input[type="text"]:focus {
+            border: solid 2px blueviolet;
+        }
+    </style>
+
+</head>
+<body>
+
+<div id="regform">
+    <form action="auth.php" method="post">
+        <fieldset>
+            <legend>Авторизация</legend>
+            <table>
+                <tr>
+                    <td><input type="text" name="id" size="30"></td>
+                    <td>Имя</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="password" size="30"></td>
+                    <td>Пароль</td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Войти">&nbsp;&nbsp;<input type="reset" value="Сброс"> </td>
+                    <td></td>
+                </tr>
+            </table>
+        </fieldset>
+    </form>
+
+</div>
+
+
+</body>
+</html>
